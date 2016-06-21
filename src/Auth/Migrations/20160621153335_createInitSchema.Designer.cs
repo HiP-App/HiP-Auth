264c5094d3e82b851af1e7cf7ae262a28be6222c
Venkatesh Penchala <venkatesh.penchala@gmail.com>
2016-06-21 17:42:06 +0200

    HiPCMS - 368 New Migrations which does not has OpenidDict related models
    Updating migrations without Openid related models.

--- src/Auth/Migrations/20160621153335_createInitSchema.Designer.cs
+++ src/Auth/Migrations/20160621153335_createInitSchema.Designer.cs
@@ -8,9 +8,10 @@ using Auth.Data;
 namespace Auth.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160621153335_createInitSchema")]
+    partial class createInitSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rc2-20901");
