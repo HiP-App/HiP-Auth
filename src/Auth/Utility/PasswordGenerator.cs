7f5f1e62271282111a6809a8f5f820cb710771f0
nagaraj1993 <nagaraj@mail.uni-paderborn.de>
2016-11-09 19:26:08 +0100

    HIPCMS - 474 - Forgot - Password

---
+++ src/Auth/Utility/PasswordGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+public class PasswordGenerator
+{
+    private char[] characterArray;
+    private Int32 passwordLength = 8;
+    private Random randNum;
+
+    public PasswordGenerator()
+    {
+        characterArray = "abcdefghijklmnopqrstuvwxyz!@$ABCDEFGHIJKLMNOPQRSTUVWXYZ!@$0123456789".ToCharArray();
+        randNum = new Random();
+    }
+
+    public string Generate()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Capacity = passwordLength;
+        for (int count = 0; count <= passwordLength - 1; count++)
+        {
+            sb.Append(GetRandomCharacter());
+        }
+        if ((sb != null))
+        {
+            return sb.ToString();
+        }
+        return string.Empty;
+    }
+
+    //Gets Random Character
+    private char GetRandomCharacter()
+    {
+        return this.characterArray[(int)((this.characterArray.GetUpperBound(0) + 1) * randNum.NextDouble())];
+    }
+}
